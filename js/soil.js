737e4b3ad3a8cc49a19be7ff212df2128c68bea6
Fajar adhitia pratama <115777980+fajaradhitiapratama@users.noreply.github.com>
2024-06-11 23:00:31 +0700

    update

--- js/soil.js
+++ js/soil.js
@@ -1,110 +1,141 @@
 tampilSoil();
 var data_soil;
-async function tampilSoil(){
+async function tampilSoil() {
+  data_soil = await getDataSoil();
 
-data_soil = await getDataSoil();
+  var silede = Object.keys(data_soil);
+  var str = '<ul id="infoList">';
+  var count = 1;
 
-var silede = Object.keys(data_soil);
-var str = '<ul id="infoList">';
-var count = 1;
+  silede.forEach(function (slide) {
+    str +=
+      '<li id="' +
+      slide +
+      '" onclick="showInfo(\'' +
+      slide +
+      "')\"> Soil Test " +
+      count +
+      "</li>";
+    count++;
+  });
 
-silede.forEach(function(slide) {
-  str += '<li id="'+slide+'" onclick="showInfo(\''+slide+'\')"> Soil Test '+count+ '</li>';
-  count++;
-})
-
-str += '</ul>';
-document.getElementById("TampilSoil").innerHTML = str;
-
+  str += "</ul>";
+  document.getElementById("TampilSoil").innerHTML = str;
 }
 
-async function getDataSoil(){
+async function getDataSoil() {
   const response = await fetch("http://localhost:8080/router/tampil_soil");
   const data = await response.json();
   return data.data;
 }
 
-async function getHistorySoil(id_alat){
-  const response = await fetch("http://localhost:8080/router/tampil_history_soil",{
-    method: "POST",
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({id: id_alat}),
-  });
+async function getHistorySoil(id_alat) {
+  const response = await fetch(
+    "http://localhost:8080/router/tampil_history_soil",
+    {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ id: id_alat }),
+    }
+  );
   const data = await response.json();
   return data.data.data;
 }
 
+
 // Fuzzy Logic Function
 function fuzzyMembership(value, min, max) {
   if (value <= min || value >= max) {
-      return 0;
+    return 0;
   }
   let mid = (min + max) / 2;
   if (value <= mid) {
-      return (value - min) / (mid - min);
+    return (value - min) / (mid - min);
   } else {
-      return (max - value) / (max - mid);
+    return (max - value) / (max - mid);
   }
 }
 
 function categorizeValues(nitrogen, phosphorous, potassium, ph, moisture) {
   const categories = {
-      'poor': {
-          nitrogen: { min: 0, max: 150 },
-          phosphorous: { min: 160, max: 200 },
-          potassium: { min: 200, max: 250 },
-          ph: { min: 0, max: 6 },
-          moisture: { min: 0, max: 20 }
-      },
-      'moderate': {
-          nitrogen: { min: 151, max: 200 },
-          phosphorous: { min: 201, max: 350 },
-          potassium: { min: 251, max: 400 },
-          ph: { min: 6, max: 7.5 },
-          moisture: { min: 20, max: 60 }
-      },
-      'good': {
-          nitrogen: { min: 201, max: 250 },
-          phosphorous: { min: 351, max: 500 },
-          potassium: { min: 401, max: 600 },
-          ph: { min: 7.5, max: 14 },
-          moisture: { min: 61, max: 80 }
-      }
+    poor: {
+      nitrogen: { min: 0, max: 150 },
+      phosphorous: { min: 160, max: 200 },
+      potassium: { min: 200, max: 250 },
+      ph: { min: 0, max: 6 },
+      moisture: { min: 0, max: 20 },
+    },
+    moderate: {
+      nitrogen: { min: 151, max: 200 },
+      phosphorous: { min: 201, max: 350 },
+      potassium: { min: 251, max: 400 },
+      ph: { min: 6, max: 7.5 },
+      moisture: { min: 20, max: 60 },
+    },
+    good: {
+      nitrogen: { min: 201, max: 250 },
+      phosphorous: { min: 351, max: 500 },
+      potassium: { min: 401, max: 600 },
+      ph: { min: 7.5, max: 14 },
+      moisture: { min: 61, max: 80 },
+    },
   };
 
   let scores = {
-      'poor': 0,
-      'moderate': 0,
-      'good': 0
+    poor: 0,
+    moderate: 0,
+    good: 0,
   };
 
   for (const category in categories) {
-      let nitrogenRange = categories[category].nitrogen;
-      let phosphorousRange = categories[category].phosphorous;
-      let potassiumRange = categories[category].potassium;
-      let phRange = categories[category].ph;
-      let moistureRange = categories[category].moisture;
+    let nitrogenRange = categories[category].nitrogen;
+    let phosphorousRange = categories[category].phosphorous;
+    let potassiumRange = categories[category].potassium;
+    let phRange = categories[category].ph;
+    let moistureRange = categories[category].moisture;
 
-      let nitrogenMembership = fuzzyMembership(nitrogen, nitrogenRange.min, nitrogenRange.max);
-      let phosphorousMembership = fuzzyMembership(phosphorous, phosphorousRange.min, phosphorousRange.max);
-      let potassiumMembership = fuzzyMembership(potassium, potassiumRange.min, potassiumRange.max);
-      let phMembership = fuzzyMembership(ph, phRange.min, phRange.max);
-      let moistureMembership = fuzzyMembership(moisture, moistureRange.min, moistureRange.max);
+    let nitrogenMembership = fuzzyMembership(
+      nitrogen,
+      nitrogenRange.min,
+      nitrogenRange.max
+    );
+    let phosphorousMembership = fuzzyMembership(
+      phosphorous,
+      phosphorousRange.min,
+      phosphorousRange.max
+    );
+    let potassiumMembership = fuzzyMembership(
+      potassium,
+      potassiumRange.min,
+      potassiumRange.max
+    );
+    let phMembership = fuzzyMembership(ph, phRange.min, phRange.max);
+    let moistureMembership = fuzzyMembership(
+      moisture,
+      moistureRange.min,
+      moistureRange.max
+    );
 
-      scores[category] += (nitrogenMembership + phosphorousMembership + potassiumMembership + phMembership + moistureMembership) / 5;
+    scores[category] +=
+      (nitrogenMembership +
+        phosphorousMembership +
+        potassiumMembership +
+        phMembership +
+        moistureMembership) /
+      5;
   }
 
   let maxScore = -1;
-  let chosenCategory = 'unknown';
+  let chosenCategory = "unknown";
 
   for (const category in scores) {
-      if (scores[category] > maxScore) {
-          maxScore = scores[category];
-          chosenCategory = category;
-      }
+    if (scores[category] > maxScore) {
+      maxScore = scores[category];
+      chosenCategory = category;
+    }
   }
 
   return chosenCategory;
@@ -112,136 +143,142 @@ function categorizeValues(nitrogen, phosphorous, potassium, ph, moisture) {
 
 // Define global variables to store markers for each soil type
 var markers = {
-soil1: null,
-soil2: null,
-soil3: null
+  soil1: null,
+  soil2: null,
+  soil3: null,
 };
 
 // Function to add marker for soil1
 function addMarkerSoil1(data) {
-removeMarkers();
-markers.soil1 = tambahkanMarker(data);
+  removeMarkers();
+  markers.soil1 = tambahkanMarker(data);
 }
 
 // Function to add marker for soil2
 function addMarkerSoil2(data) {
-removeMarkers();
-markers.soil2 = tambahkanMarker(data);
+  removeMarkers();
+  markers.soil2 = tambahkanMarker(data);
 }
 
 // Function to add marker for soil3
 function addMarkerSoil3(data) {
-removeMarkers();
-markers.soil3 = tambahkanMarker(data);
+  removeMarkers();
+  markers.soil3 = tambahkanMarker(data);
 }
 
 // Function to remove markers for all soil types
 function removeMarkers() {
-if (markers.soil1) {
-  map.removeLayer(markers.soil1);
-  markers.soil1 = null;
-}
-if (markers.soil2) {
-  map.removeLayer(markers.soil2);
-  markers.soil2 = null;
-}
-if (markers.soil3) {
-  map.removeLayer(markers.soil3);
-  markers.soil3 = null;
-}
+  if (markers.soil1) {
+    map.removeLayer(markers.soil1);
+    markers.soil1 = null;
+  }
+  if (markers.soil2) {
+    map.removeLayer(markers.soil2);
+    markers.soil2 = null;
+  }
+  if (markers.soil3) {
+    map.removeLayer(markers.soil3);
+    markers.soil3 = null;
+  }
 }
 
 // Function to add marker with popup
 function tambahkanMarker(data) {
-// Determine soil category using fuzzy logic
-const category = categorizeValues(data.n, data.p, data.k, data.ph, data.moisture);
+  // Determine soil category using fuzzy logic
+  const category = categorizeValues(
+    data.n,
+    data.p,
+    data.k,
+    data.ph,
+    data.moisture
+  );
 
-// Tentukan warna ikon berdasarkan kategori tanah
-var iconColor;
-if (category === 'poor') {
-  iconColor = "red";
-} else if (category === 'moderate') {
-  iconColor = "yellow";
-} else {
-  iconColor = "green";
-}
+  // Tentukan warna ikon berdasarkan kategori tanah
+  var iconColor;
+  if (category === "poor") {
+    iconColor = "red";
+  } else if (category === "moderate") {
+    iconColor = "yellow";
+  } else {
+    iconColor = "green";
+  }
 
-// Create marker with popup
-var marker = L.marker([data.LAT, data.LONG], {
-  icon: L.divIcon({
-    className: "custom-icon " + iconColor,
-    iconSize: [15, 15],
-  }),
-})
-.bindPopup(
-  `
+  // Create marker with popup
+  var marker = L.marker([data.LAT, data.LONG], {
+    icon: L.divIcon({
+      className: "custom-icon " + iconColor,
+      iconSize: [15, 15],
+    }),
+  })
+    .bindPopup(
+      `
   <b>Alat ${data.alat}</b><br>
   N: ${data.n}<br>
   P: ${data.p}<br>
   K: ${data.k}<br>
   pH: ${data.ph}<br>
   Moisture: ${data.moisture}`
-)
-.addTo(map);
+    )
+    .addTo(map);
 
-// Return the created marker
-return marker;
+  // Return the created marker
+  return marker;
 }
 
 // change value
 async function showInfo(type) {
-  var data_history = await getHistorySoil(type)
-  data_history = data_history[data_history.length-1]
-  console.log(data_soil[type])
-  
-  
+  var data_history = await getHistorySoil(type);
+  data_history = data_history[data_history.length - 1];
+  console.log(data_soil[type]);
+
   const infoListItem = document.querySelectorAll("#infoList li");
   infoListItem.forEach((item) => item.classList.remove("active"));
-  
+
   const clickedItem = document.getElementById(type);
   clickedItem.classList.add("active");
-    var alat = {
-      alat: data_soil[type].jenis_iot,
-      LAT: data_soil[type].lat,
-      LONG: data_soil[type].long,
-      n: data_history.N, 
-      p: data_history.P,
-      k: data_history.K,
-      ph: data_history.PH,
-      moisture: data_history.mosit  
-    };   
-    document.getElementById("infoMoisture").textContent = alat.moisture + " %";
-    document.getElementById("infopH").textContent = alat.ph;
-    document.getElementById("infoN").textContent = alat.n;
-    document.getElementById("infoP").textContent = alat.p;
-    document.getElementById("infoK").textContent = alat.k ;
-    document.getElementById("coordinateLat").textContent = alat.LAT;
-    document.getElementById("coordinateLong").textContent = alat.LONG;
-    addMarkerSoil1(alat); 
+  var alat = {
+    alat: data_soil[type].jenis_iot,
+    LAT: data_soil[type].lat,
+    LONG: data_soil[type].long,
+    n: data_history.N,
+    p: data_history.P,
+    k: data_history.K,
+    ph: data_history.PH,
+    moisture: data_history.mosit,
+  };
+  document.getElementById("infoMoisture").textContent = alat.moisture + " %";
+  document.getElementById("infopH").textContent = alat.ph;
+  document.getElementById("infoN").textContent = alat.n;
+  document.getElementById("infoP").textContent = alat.p;
+  document.getElementById("infoK").textContent = alat.k;
+  document.getElementById("coordinateLat").textContent = alat.LAT;
+  document.getElementById("coordinateLong").textContent = alat.LONG;
+  addMarkerSoil1(alat);
+
+  map.setView([alat.LAT, alat.LONG], 18);
 }
 
 // navbar
 function toggleActiveClass(element) {
-var myList = document.getElementById("myList");
-var listItems = myList.getElementsByTagName("li");
+  var myList = document.getElementById("myList");
+  var listItems = myList.getElementsByTagName("li");
 
-// Menonaktifkan class "active" dari semua elemen <li>
-for (var i = 0; i < listItems.length; i++) {
-  listItems[i].getElementsByTagName("a")[0].classList.remove("active");
-}
+  // Menonaktifkan class "active" dari semua elemen <li>
+  for (var i = 0; i < listItems.length; i++) {
+    listItems[i].getElementsByTagName("a")[0].classList.remove("active");
+  }
 
-// Mengaktifkan class "active" pada elemen yang diklik
-element.classList.add("active");
+  // Mengaktifkan class "active" pada elemen yang diklik
+  element.classList.add("active");
 }
 
 // pop up data
 function myFunction(event) {
-var popup = event.currentTarget.querySelector(".popuptext");
-popup.classList.toggle("show");
+  var popup = event.currentTarget.querySelector(".popuptext");
+  popup.classList.toggle("show");
 }
 
 // pop up map
-function movemap(){
-window.location.href = "map_ta.html";
+function movemap() {
+  window.location.href = "map_ta.html";
 }
-
