f3fdea057ef6dac9907a3fba172d48dc6cd5a4bf
Fajar adhitia pratama <115777980+fajaradhitiapratama@users.noreply.github.com>
2024-06-16 19:38:54 +0700

    update

--- js/soil.js
+++ js/soil.js
@@ -69,11 +69,11 @@ function fuzzyMembership(value, min, max) {
 function categorizeValues(nitrogen, phosphorous, potassium, ph, moisture) {
   const categories = {
     poor: {
-      nitrogen: { min: 0, max: 150 },
-      phosphorous: { min: 0, max: 6 },
-      potassium: { min: 0, max: 65 },
-      ph: { min: 0, max: 6 },
-      moisture: { min: 0, max: 20 },
+      nitrogen: { min: 0, max: 149 },
+      phosphorous: { min: 0, max: 5 },
+      potassium: { min: 0, max: 64 },
+      ph: { min: 0, max: 5 },
+      moisture: { min: 0, max: 19 },
     },
     moderate: {
       nitrogen: { min: 150, max: 200 },
@@ -83,11 +83,11 @@ function categorizeValues(nitrogen, phosphorous, potassium, ph, moisture) {
       moisture: { min: 20, max: 60 },
     },
     good: {
-      nitrogen: { min: 200, max: 300 },
-      phosphorous: { min: 12, max: 300 },
-      potassium: { min: 155, max: 300 },
-      ph: { min: 7.5, max: 14 },
-      moisture: { min: 60, max: 100 },
+      nitrogen: { min: 201, max: 300 },
+      phosphorous: { min: 13, max: 300 },
+      potassium: { min: 156, max: 300 },
+      ph: { min: 7.6, max: 14 },
+      moisture: { min: 61, max: 100 },
     },
   };
 
