8b09c3400c400a02430b21e3a870c5c86b05b066
Fajar adhitia pratama <115777980+fajaradhitiapratama@users.noreply.github.com>
2024-06-16 19:03:31 +0700

    update

---
+++ js/soil.js
@@ -0,0 +1,296 @@
+tampilSoil();
+var data_soil;
+
+async function tampilSoil() {
+  data_soil = await getDataSoil();
+
+  var silede = Object.keys(data_soil);
+  var str = '<ul id="infoList">';
+  var count = 1;
+
+  silede.forEach(function (slide, index) {
+    str +=
+      '<li id="' +
+      slide +
+      '" class="' + (index === 0 ? 'active' : '') + '" onclick="showInfo(\'' +
+      slide +
+      "')\"> Soil Test " +
+      count +
+      "</li>";
+    count++;
+  });
+
+  str += "</ul>";
+  document.getElementById("TampilSoil").innerHTML = str;
+
+  // Panggil showInfo untuk item pertama untuk menampilkan detailnya secara default
+  if (silede.length > 0) {
+    showInfo(silede[0]);
+  }
+}
+
+async function getDataSoil() {
+  const response = await fetch(
+    "https://backend-agridrone.vercel.app/router/tampil_soil/"
+  );
+  const data = await response.json();
+  return data.data;
+}
+
+async function getHistorySoil(id_alat) {
+  const response = await fetch(
+    "https://backend-agridrone.vercel.app/router/tampil_history_soil/",
+    {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ id: id_alat }),
+    }
+  );
+  const data = await response.json();
+  return data.data.data;
+}
+
+// Fungsi logika Fuzzy
+function fuzzyMembership(value, min, max) {
+  if (value <= min || value >= max) {
+    return 0;
+  }
+  let mid = (min + max) / 2;
+  if (value <= mid) {
+    return (value - min) / (mid - min);
+  } else {
+    return (max - value) / (max - mid);
+  }
+}
+
+function categorizeValues(nitrogen, phosphorous, potassium, ph, moisture) {
+  const categories = {
+    poor: {
+      nitrogen: { min: 0, max: 150 },
+      phosphorous: { min: 0, max: 6 },
+      potassium: { min: 0, max: 65 },
+      ph: { min: 0, max: 6 },
+      moisture: { min: 0, max: 20 },
+    },
+    moderate: {
+      nitrogen: { min: 150, max: 200 },
+      phosphorous: { min: 6, max: 12 },
+      potassium: { min: 65, max: 155 },
+      ph: { min: 6, max: 7.5 },
+      moisture: { min: 20, max: 60 },
+    },
+    good: {
+      nitrogen: { min: 200, max: 1000 },
+      phosphorous: { min: 12, max: 1000 },
+      potassium: { min: 155, max: 1000 },
+      ph: { min: 7.5, max: 1000 },
+      moisture: { min: 60, max: 1000 },
+    },
+  };
+
+  let scores = {
+    poor: 0,
+    moderate: 0,
+    good: 0,
+  };
+
+  for (const category in categories) {
+    let nitrogenRange = categories[category].nitrogen;
+    let phosphorousRange = categories[category].phosphorous;
+    let potassiumRange = categories[category].potassium;
+    let phRange = categories[category].ph;
+    let moistureRange = categories[category].moisture;
+
+    let nitrogenMembership = fuzzyMembership(
+      nitrogen,
+      nitrogenRange.min,
+      nitrogenRange.max
+    );
+    let phosphorousMembership = fuzzyMembership(
+      phosphorous,
+      phosphorousRange.min,
+      phosphorousRange.max
+    );
+    let potassiumMembership = fuzzyMembership(
+      potassium,
+      potassiumRange.min,
+      potassiumRange.max
+    );
+    let phMembership = fuzzyMembership(ph, phRange.min, phRange.max);
+    let moistureMembership = fuzzyMembership(
+      moisture,
+      moistureRange.min,
+      moistureRange.max
+    );
+
+    scores[category] +=
+      (nitrogenMembership +
+        phosphorousMembership +
+        potassiumMembership +
+        phMembership +
+        moistureMembership) /
+      5;
+  }
+
+  let maxScore = -1;
+  let chosenCategory = "unknown";
+
+  for (const category in scores) {
+    if (scores[category] > maxScore) {
+      maxScore = scores[category];
+      chosenCategory = category;
+    }
+  }
+
+  return chosenCategory;
+}
+
+// Variabel global untuk menyimpan marker untuk setiap jenis tanah
+var markers = {
+  soil1: null,
+  soil2: null,
+  soil3: null,
+};
+
+// Fungsi untuk menambahkan marker untuk tanah1
+function addMarkerSoil1(data) {
+  removeMarkers();
+  markers.soil1 = tambahkanMarker(data);
+}
+
+// Fungsi untuk menambahkan marker untuk tanah2
+function addMarkerSoil2(data) {
+  removeMarkers();
+  markers.soil2 = tambahkanMarker(data);
+}
+
+// Fungsi untuk menambahkan marker untuk tanah3
+function addMarkerSoil3(data) {
+  removeMarkers();
+  markers.soil3 = tambahkanMarker(data);
+}
+
+// Fungsi untuk menghapus marker untuk semua jenis tanah
+function removeMarkers() {
+  if (markers.soil1) {
+    map.removeLayer(markers.soil1);
+    markers.soil1 = null;
+  }
+  if (markers.soil2) {
+    map.removeLayer(markers.soil2);
+    markers.soil2 = null;
+  }
+  if (markers.soil3) {
+    map.removeLayer(markers.soil3);
+    markers.soil3 = null;
+  }
+}
+
+// Fungsi untuk menambahkan marker dengan popup
+function tambahkanMarker(data) {
+  // Tentukan kategori tanah menggunakan logika Fuzzy
+  const category = categorizeValues(
+    data.n,
+    data.p,
+    data.k,
+    data.ph,
+    data.moisture
+  );
+
+  // Tentukan warna ikon berdasarkan kategori tanah
+  var iconColor;
+  if (category === "poor") {
+    iconColor = "red";
+  } else if (category === "moderate") {
+    iconColor = "yellow";
+  } else {
+    iconColor = "green";
+  }
+
+  // Buat marker dengan popup
+  var marker = L.marker([data.LAT, data.LONG], {
+    icon: L.divIcon({
+      className: "custom-icon " + iconColor,
+      iconSize: [15, 15],
+    }),
+  })
+    .bindPopup(
+      `
+  <b>Alat ${data.alat}</b><br>
+  N: ${data.n}<br>
+  P: ${data.p}<br>
+  K: ${data.k}<br>
+  pH: ${data.ph}<br>
+  Moisture: ${data.moisture}`
+    )
+    .addTo(map);
+
+  // Kembalikan marker yang dibuat
+  return marker;
+}
+
+// Fungsi untuk mengatur class aktif pada item daftar yang diklik
+function setActiveClass(type) {
+  const infoListItem = document.querySelectorAll("#infoList li");
+  infoListItem.forEach((item) => item.classList.remove("active"));
+
+  const clickedItem = document.getElementById(type);
+  clickedItem.classList.add("active");
+}
+
+// ganti nilai
+async function showInfo(type) {
+  var data_history = await getHistorySoil(type);
+  data_history = data_history[data_history.length - 1];
+
+  setActiveClass(type);  // Set class active
+
+  var alat = {
+    alat: data_soil[type].jenis_iot,
+    LAT: data_soil[type].lat,
+    LONG: data_soil[type].long,
+    n: data_history.N,
+    p: data_history.P,
+    k: data_history.K,
+    ph: data_history.PH,
+    moisture: data_history.mosit,
+  };
+  document.getElementById("infoMoisture").textContent = alat.moisture + " %";
+  document.getElementById("infopH").textContent = alat.ph;
+  document.getElementById("infoN").textContent = alat.n;
+  document.getElementById("infoP").textContent = alat.p;
+  document.getElementById("infoK").textContent = alat.k;
+  document.getElementById("coordinateLat").textContent = alat.LAT;
+  document.getElementById("coordinateLong").textContent = alat.LONG;
+  addMarkerSoil1(alat);
+
+  map.setView([alat.LAT, alat.LONG], 18);
+}
+
+// navbar
+function toggleActiveClass(element) {
+  var myList = document.getElementById("myList");
+  var listItems = myList.getElementsByTagName("li");
+
+  // Menonaktifkan class "active" dari semua elemen <li>
+  for (var i = 0; i < listItems.length; i++) {
+    listItems[i].getElementsByTagName("a")[0].classList.remove("active");
+  }
+
+  // Mengaktifkan class "active" pada elemen yang diklik
+  element.classList.add("active");
+}
+
+// pop up data
+function myFunction(event) {
+  var popup = event.currentTarget.querySelector(".popuptext");
+  popup.classList.toggle("show");
+}
+
+// pop up map
+function movemap() {
+  window.location.href = "map_ta.html";
+}
